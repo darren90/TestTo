3198c035c35490e5fa9d4867b419d62de00d6ce9
darren90 <1005052145@qq.com>
2016-07-05 20:39:02 +0800

    add

--- autohome/models.py
+++ autohome/models.py
@@ -1,25 +1,6 @@
 from app import db
 import  MySQLdb
 
-# class Todo(db.Model):
-#     content = db.Column(db.String)
-#     time = db.Column(db.String)
-#     status = db.Column(db.Boolean)
-#
-#     def __init__(self,content,time):
-#         self.content = content
-#         self.time = time
-#
-#
-#     def to_json(self):]
-#         return {
-#             'id': str(self.id),
-#             'content': self.content,
-#             'time': self.time,
-#             'status': self.status
-#         }
-
-
 class User(db.Model):
     user_id = db.Column(db.Integer,primary_key=True)
     user_name = db.Column(db.String)
