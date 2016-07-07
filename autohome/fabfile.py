16392196fbec3eece6656cbeaa05002402cafe78
darren90 <1005052145@qq.com>
2016-07-08 00:08:08 +0800

    add

--- autohome/fabfile.py
+++ autohome/fabfile.py
@@ -1,3 +1,5 @@
+#-*- coding: UTF-8 -*-
+
 from  fabric.api import *
 
 env.hosts = ['121.42.203.41']
@@ -8,6 +10,15 @@ env.password = '@Tengfei05'
 def hello():
     print "hello fabric"
 
+# 只是拉取代码而已
+def pullgit():
+    with cd('/home/tengfei/TestTo'):
+        # run('git pull')
+        run('git fetch --all')
+        run('git reset --hard origin/master ')
+        run('git pull')
+
+# 部署应用
 def deploy():
     with cd('/home/tengfei/TestTo'):
         # run('git pull')
