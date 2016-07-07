5d461d6973bb997b6ebf2de9b532b5733c5fb737
darren90 <1005052145@qq.com>
2016-07-07 23:41:52 +0800

    add

--- autohome/spider_car/car.py
+++ autohome/spider_car/car.py
@@ -41,6 +41,47 @@ class spider(object):
             count = count + 1
         return pages_arary
 
+    # 解析全部的品牌信息
+    def parse_html_data_brand(self,html_countent,url):
+
+        soup = BeautifulSoup(html_countent, "html.parser", from_encoding='gb2312')
+        brands = soup.find('div', class_="tab-content-item current")
+        print brands
+        if brands is None:
+            return []
+
+        for sbrand in brands.find_all('div'):
+            brand_class = sbrand.find('div', class_="tab-content-item current")
+
+            print brand_class.find('span').get_text()
+
+
+            # # 帖子的详情地址
+            # detail_url = new_link['href']
+            #
+            # # 帖子的标题
+            # title = new_link['title']
+            #
+            # # 图片地址
+            # icon_url = new_link.find('img')['src']
+            #
+            # # 副标题
+            # sub_title = sbrand.find('span').get_text()
+            #
+            # # 发帖人id
+            # bbs_id = sbrand.find('dt').find('a').get_text()
+            #
+            # bbs__ = sbrand.find('dd', class_="font12NoLine").find('a')
+            #
+            # # 文章所在的论坛名字
+            # bbs_name = bbs__.get_text()
+            #
+            # # 文章所在轮胎的地址
+            # bbs_url = urlparse.urljoin("http://club.autohome.com.cn", bbs__['href'])
+            #
+            # user = User(title, detail_url, icon_url, bbs_id, bbs_name, sub_title, bbs_url)
+            # user.save()
+
 
 #解析具体的数据
     def parse_html_data(self,html_countent,url):
@@ -98,22 +139,24 @@ def runapp():
     fialCount = 1
     count = 1
     classinfo = []
-    url = 'http://club.autohome.com.cn/JingXuan/104/'
+    url = 'http://www.autohome.com.cn/car/?pvareaid=101452'
     mySpider = spider()
-    all_pages = mySpider.changepage(url,20)
-    # all_links = mySpider.getAllLines(all_pages)
-    for link in all_pages:
-        print link
-        try:
-            html = mySpider.getsource(link)
-            mySpider.parse_html_data(html,link)
-            count = count +1
-        except Exception as e:
-            if fialCount == 10200:
-                fout.close()
-                break
-            fialCount = fialCount + 1
-            count = count +1
+    html = mySpider.getsource(url)
+    mySpider.parse_html_data_brand(html, url)
+    # all_pages = mySpider.changepage(url,20)
+    # # all_links = mySpider.getAllLines(all_pages)
+    # for link in all_pages:
+    #     print link
+    #     try:
+    #         html = mySpider.getsource(link)
+    #         mySpider.parse_html_data(html,link)
+    #         count = count +1
+    #     except Exception as e:
+    #         if fialCount == 10200:
+    #             fout.close()
+    #             break
+    #         fialCount = fialCount + 1
+    #         count = count +1
 
 
 # 配置爬虫，每10个小时爬一次
