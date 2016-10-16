ee815f2255ce088e62b8b0e3bcc8ac2746058f45
darren90 <1005052145@qq.com>
2016-10-16 12:35:48 +0800

    add

--- autotest/__init__.py
+++ autotest/__init__.py
@@ -3,3 +3,7 @@ http://www.jianshu.com/p/1b3adaa76c61
 http://www.cocoachina.com/ios/20160307/15501.html
 
 https://github.com/xx-li/iOSAutoPackaging
+
+http://ios.jobbole.com/88144/
+
+https://segmentfault.com/a/1190000004533963
