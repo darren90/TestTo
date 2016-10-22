1bd5e0ecdf9d19f30ecaac4cb96dcd3e83ea0c94
Fengtf <1005052145@qq.com>
2016-10-22 15:35:14 +0800

    add

--- autotest/__init__.py
+++ autotest/__init__.py
@@ -1,9 +1,9 @@
-http://www.jianshu.com/p/1b3adaa76c61
-
-http://www.cocoachina.com/ios/20160307/15501.html
-
-https://github.com/xx-li/iOSAutoPackaging
-
-http://ios.jobbole.com/88144/
-
-https://segmentfault.com/a/1190000004533963
+# http://www.jianshu.com/p/1b3adaa76c61
+#
+# http://www.cocoachina.com/ios/20160307/15501.html
+#
+# https://github.com/xx-li/iOSAutoPackaging
+#
+# http://ios.jobbole.com/88144/
+#
+# https://segmentfault.com/a/1190000004533963
